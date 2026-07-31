5da2d1a80ccb19dda27e58b987c71b6d9613576c
Amna Fatima <amnafatimaa6@gmail.com>
2026-07-31 06:10:09 -0700

    Add physical constants to constants.py

---
+++ utils/constants.py
@@ -0,0 +1,17 @@
+"""
+Physical constants used throughout the project.
+"""
+
+import numpy as np
+
+# Speed of light (m/s)
+C = 299792458
+
+# Gravitational constant
+G = 6.67430e-11
+
+# Solar Mass
+SOLAR_MASS = 1.98847e30
+
+# Pi
+PI = np.pi
